c09c8f8d92425e2aeb312b95340d16248053a112
Nyoman Sunima <nyomansunima@gmail.com>
2025-07-10 16:37:41 +0800

    feat: add connections file

---
+++ src/shared/libs/connections.ts
@@ -0,0 +1,13 @@
+import axios from "axios"
+import { loadConfig } from "./config"
+
+const config = loadConfig()
+
+export const kitClient = axios.create({
+  baseURL: "https://api.kit.com/v4",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    "X-Kit-Api-Key": config.kit.key,
+  },
+})
