6625953d0880a51ee85b6975c701e99e44322c4c
Nyoman Sunima <nyomansunima@gmail.com>
2025-07-10 12:23:51 +0800

    refactor: add work service test

--- src/features/works/work-service.spec.ts
+++ src/features/works/work-service.spec.ts
@@ -1,5 +1,27 @@
-import { describe, test } from "vitest"
+import { describe, expect, test, it } from "vitest"
+import { getWorkDetail, getWorks } from "./work-service"
 
 describe("Work Service", () => {
-  test("Get all works", () => {})
+  describe("All works", () => {
+    test("Should not null", async () => {
+      const works = await getWorks()
+      expect(works).not.toBeNull()
+      expect(works.length).not.toBeNull()
+    })
+  })
+
+  describe("Work detail", () => {
+    test("Should not null", async () => {
+      const slug = "dev-crates"
+      const work = await getWorkDetail(slug)
+      expect(work).not.toBeNull()
+    })
+
+    test("Get work detail", async () => {
+      const slug = "dev-crates"
+      const work = await getWorkDetail(slug)
+      expect(work.meta.title).not.toBeNull()
+      expect(work.meta.title).not.toEqual("")
+    })
+  })
 })
