592241ecf0ab7303dc95dcbac1aa6f723fa8549c
Nyoman Sunima <nyomansunima@gmail.com>
2025-07-17 15:01:08 +0800

    feat: add hono server

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite"
+import { reactRouterHonoServer } from "react-router-hono-server/dev"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    reactRouterHonoServer(),
+    reactRouter(),
+    tailwindcss(),
+    tsconfigPaths(),
+  ],
 })
