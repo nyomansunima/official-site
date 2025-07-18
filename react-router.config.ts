428355764e04b70a7c704864f8ee02b55a5aa532
Nyoman Sunima <nyomansunima@gmail.com>
2025-07-18 21:31:19 +0800

    fix: add the static paths for prerender

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,28 @@
+import fs from "fs/promises"
+import path from "path"
 import type { Config } from "@react-router/dev/config"
 
+async function getWorkPaths(): Promise<string[]> {
+  const workDirPath = path.join(process.cwd(), "src/contents/works")
+  const files = await fs.readdir(workDirPath)
+
+  const slugs = files
+    .filter((file) => file.endsWith(".md"))
+    .map((file) => {
+      const slug = file.replace(/\.(md)$/, "")
+      return `/works/${slug}`
+    }) as string[]
+
+  return slugs
+}
+
 export default {
-  appDirectory: "src",
+  appDirectory: "src/app",
   ssr: true,
-  prerender: true,
+  prerender: async ({ getStaticPaths }) => {
+    const staticPaths = getStaticPaths()
+    const workPaths = await getWorkPaths()
+
+    return [...staticPaths, ...workPaths]
+  },
 } satisfies Config
