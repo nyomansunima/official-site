057fdb831bcdea26c3ead8f255d851402e970f29
Nyoman Sunima <nyomansunima@gmail.com>
2025-07-22 21:37:12 +0800

    refactor(style): combine less style

--- src/features/crafts/craft-item.tsx
+++ src/features/crafts/craft-item.tsx
@@ -17,18 +17,16 @@ export function CraftItem({ craft }: Props) {
     <Link
       to={link}
       target="_blank"
-      className="flex p-1 rounded-2xl bg-surface relative group transition-all duration-300 hover:-translate-y-1 group"
+      className="flex flex-col bg-surface p-3 rounded-2xl border-2 border-border border-dashed transition-all duration-300 hover:-translate-y-1 hover:border-link cursor-pointer"
     >
-      <div className="flex flex-col h-full w-full p-3 rounded-xl border-2 border-border border-dashed transition-all duration-300 hover:border-link">
-        <div className="flex gap-2">
-          <i className="fi fi-sc-check-circle group-hover:text-link text-sm" />
-          <h3 className="text-sm font-medium">{title}</h3>
-        </div>
-
-        <p className="text-sm mt-3 text-foreground/60 line-clamp-2 text-pretty leading-relaxed">
-          {description}
-        </p>
+      <div className="flex gap-2">
+        <i className="fi fi-sc-check-circle text-sm" />
+        <h3 className="text-sm font-medium">{title}</h3>
       </div>
+
+      <p className="text-sm mt-3 text-foreground/60 line-clamp-2 text-pretty leading-relaxed">
+        {description}
+      </p>
     </Link>
   )
 }
