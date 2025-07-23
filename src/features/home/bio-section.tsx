0dfc18dd7027d7bc39dd8adf8341e21e941ce5f3
Nyoman Sunima <nyomansunima@gmail.com>
2025-07-23 23:53:12 +0800

    refactor: remove text opacity

--- src/features/home/bio-section.tsx
+++ src/features/home/bio-section.tsx
@@ -14,7 +14,7 @@ export function BioSection() {
         <h2 className="py-2 text-lg font-medium">Explore good things.</h2>
       </div>
 
-      <p className="mt-6 text-pretty leading-7 text-foreground/60">
+      <p className="mt-6 text-pretty leading-7">
         Check something that make you feel better or see what's i have built for
         you. Check it now.
       </p>
