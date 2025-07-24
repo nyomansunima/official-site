3cea8114e2d8c2c5394d5013979579c1b66d1e35
Nyoman Sunima <nyomansunima@gmail.com>
2025-07-24 16:20:21 +0800

    refactor: add description

--- src/features/support/support-section.tsx
+++ src/features/support/support-section.tsx
@@ -4,7 +4,7 @@ import supports from "./support-data.json"
 export function SupportSection() {
   return (
     <section className="flex">
-      <div className="flex flex-col gap-10">
+      <div className="flex flex-col">
         <div className="flex flex-col gap-2">
           <span className="text-sm text-link font-mono tracking-tight">
             Support.
@@ -12,7 +12,12 @@ export function SupportSection() {
           <h2 className="text-lg font-medium">Help me out.</h2>
         </div>
 
-        <div className="grid grid-cols-1 tablet:grid-cols-2 gap-3">
+        <p className="mt-6 text-pretty leading-7">
+          Support my works, open source projects that i've working on. You can
+          give me a coffee to speed up.
+        </p>
+
+        <div className="grid grid-cols-1 tablet:grid-cols-2 gap-3 mt-10">
           {supports.map((support, index) => (
             <SupportItem support={support} key={index} />
           ))}
