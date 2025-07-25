a4ac97be83adef52fc63d813c47d40a0ad406642
Nyoman Sunima <nyomansunima@gmail.com>
2025-07-25 22:42:59 +0800

    refactor: add description

--- src/features/journeys/log-section.tsx
+++ src/features/journeys/log-section.tsx
@@ -6,7 +6,19 @@ const logGroups = [...journeysData].reverse()
 export function LogSection() {
   return (
     <section className="flex flex-col">
-      <div className="flex flex-col gap-14">
+      <div className="flex flex-col gap-2">
+        <span className="text-sm text-link font-mono tracking-tight">
+          Changelog.
+        </span>
+        <h2 className="text-lg font-medium">What's going on?</h2>
+      </div>
+
+      <p className="mt-6 text-pretty leading-7">
+        My recent activities, achievements, updates and what i'am built. Also
+        the latest updates about my products.
+      </p>
+
+      <div className="flex flex-col gap-14 mt-10">
         {logGroups.map((group, groupIndex) => (
           <LogGroup group={group} key={groupIndex} />
         ))}
