45321b36e5ecd9234f7173441c290e1af8fdb7f5
Nyoman Sunima <nyomansunima@gmail.com>
2025-07-25 22:42:33 +0800

    refactor: add description

--- src/features/journeys/experience-section.tsx
+++ src/features/journeys/experience-section.tsx
@@ -6,15 +6,20 @@ const experiences = [...experienceData].reverse()
 export function ExperienceSection() {
   return (
     <section className="flex flex-col">
-      <div className="flex flex-col gap-10">
+      <div className="flex flex-col">
         <div className="flex flex-col gap-2">
           <span className="text-sm text-link font-mono tracking-tight">
-            Journeys.
+            Experiences.
           </span>
-          <h2 className="text-lg font-medium">What's going on?</h2>
+          <h2 className="text-lg font-medium">My careers and journeys.</h2>
         </div>
 
-        <div className="flex flex-col gap-3 bg-surface rounded-2xl p-5 border-2 border-border border-dashed transition-all duration-300 hover:border-link hover:-translate-y-1 cursor-pointer">
+        <p className="mt-6 text-pretty leading-7">
+          My overall contributions, leading and participants to create and ship
+          a good products.
+        </p>
+
+        <div className="flex flex-col gap-3 mt-10 bg-surface rounded-2xl p-5 border-2 border-border border-dashed transition-all duration-300 hover:border-link cursor-pointer">
           {experiences.map((exp, index) => (
             <ExperienceItem experience={exp} key={index} />
           ))}
