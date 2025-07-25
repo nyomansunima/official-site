cbd0414b7d1a460e7682828057a1c8c55cdac8db
Nyoman Sunima <nyomansunima@gmail.com>
2025-07-25 22:44:53 +0800

    refactor: add description

--- src/features/stories/publication-section.tsx
+++ src/features/stories/publication-section.tsx
@@ -3,7 +3,7 @@ import publications from "./publication-data.json"
 
 export function PublicationSection() {
   return (
-    <section className="flex flex-col gap-10">
+    <section className="flex flex-col">
       <div className="flex flex-col gap-2">
         <span className="text-sm text-link font-mono tracking-tight">
           Publications.
@@ -11,7 +11,12 @@ export function PublicationSection() {
         <h2 className="text-lg font-medium">Where's the stories?</h2>
       </div>
 
-      <div className="grid grid-cols-2 tablet:grid-cols-3 gap-2 w-full">
+      <p className="mt-6 text-pretty leading-7">
+        My favorite place to share and publicly updates about news, tips and
+        tricks, blogs and newsletter. Check this out.
+      </p>
+
+      <div className="grid grid-cols-2 tablet:grid-cols-3 gap-2 w-full mt-10">
         {publications.map((pub, index) => (
           <PublicationItem publication={pub} key={index} />
         ))}
--- src/features/stories/recent-section.tsx
+++ src/features/stories/recent-section.tsx
@@ -5,7 +5,7 @@ const recents = [...storiesData].reverse()
 
 export function RecentlySection() {
   return (
-    <section className="flex flex-col gap-10">
+    <section className="flex flex-col">
       <div className="flex flex-col gap-2">
         <span className="text-sm text-link font-mono tracking-tight">
           Recents.
@@ -13,7 +13,12 @@ export function RecentlySection() {
         <h2 className="text-lg font-medium">Just now.</h2>
       </div>
 
-      <div className="flex flex-col gap-2">
+      <p className="mt-6 text-pretty leading-7">
+        My recents updates, see my articles, video, podcasts and my recent
+        publications about anything.
+      </p>
+
+      <div className="flex flex-col gap-2 mt-10">
         {recents.map((recent, i) => (
           <RecentItem recent={recent} key={i} />
         ))}
