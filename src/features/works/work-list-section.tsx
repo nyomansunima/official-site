fb287dee690938bb21da7551da82ac2f24bc646a
Nyoman Sunima <nyomansunima@gmail.com>
2025-08-03 17:29:02 +0800

    refactor: remove desc position

--- src/features/works/work-list-section.tsx
+++ src/features/works/work-list-section.tsx
@@ -8,19 +8,19 @@ interface Props {
 export function WorksListSection({ works }: Props) {
   return (
     <section className="flex flex-col">
-      <div className="flex flex-col gap-2">
-        <span className="text-sm text-link font-mono tracking-tight">
+      <div className="flex flex-col">
+        <span className="text-sm text-foreground/40 font-mono tracking-tight">
           Works.
         </span>
-        <h2 className="text-lg font-medium">Top built & hacking.</h2>
+        <h2 className="text-lg font-medium mt-2">Top built & hacking.</h2>
+
+        <p className="mt-6 text-pretty leading-7">
+          I’ve developed commercial projects as well as hobby projects. All
+          projects are included (business, works & case studies).
+        </p>
       </div>
 
-      <p className="mt-6 text-pretty leading-7">
-        I’ve developed commercial projects as well as hobby projects. All
-        projects are included (business, works, case studies).
-      </p>
-
-      <div className="flex flex-col gap-3 mt-10">
+      <div className="flex flex-col gap-16 mt-10">
         {works.map((work, i) => (
           <WorkItem work={work} key={i} />
         ))}
