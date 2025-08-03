a55b9de7d5052748d267b6d7264d100cab9e9622
Nyoman Sunima <nyomansunima@gmail.com>
2025-08-03 17:27:47 +0800

    fix: remove hover effect

--- src/shared/components/ui/input.tsx
+++ src/shared/components/ui/input.tsx
@@ -13,7 +13,7 @@ const Input = ({
     <input
       type={type}
       className={mergeClass(
-        "flex grow h-10 w-full text-sm rounded-xl border-2 border-border border-dashed ring-0 outline-none bg-background text-foreground px-3 py-2 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-foreground/60 focus-visible:outline-none focus-visible:border-link disabled:cursor-not-allowed disabled:opacity-50",
+        "flex grow h-10 w-full text-sm rounded-xl border-2 border-border border-dashed ring-0 outline-none bg-background text-foreground px-3 py-2 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-foreground/60 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
         className,
       )}
       ref={ref}
