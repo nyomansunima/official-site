cbf7d3acbf24981c52eb28f7660bb596a2b8a6ca
Nyoman Sunima <nyomansunima@gmail.com>
2025-08-03 17:29:28 +0800

    refactor: remove unused props and component

--- src/features/works/work-detail-content.tsx
+++ src/features/works/work-detail-content.tsx
@@ -1,11 +1,4 @@
-import {
-  ArticleContent,
-  MarkdownContent,
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@shared/components"
+import { ArticleContent, MarkdownContent } from "@shared/components"
 import { parseReadableDate } from "@shared/libs"
 import { Link } from "react-router"
 import type { WorkDetail } from "./work-service"
@@ -15,49 +8,27 @@ interface WorkDetailContentProps {
 }
 
 export function WorkDetailContent({ work }: WorkDetailContentProps) {
-  const { category, date, status, team, title, type } = work.meta
+  const { date, team, title } = work.meta
   const parsedDate = parseReadableDate(date)
 
   return (
     <div className="flex flex-col">
-      <h1 className="text-xl font-medium text-balance">{title}</h1>
+      <h1 className="text-lg font-medium text-balance">{title}</h1>
 
       <div className="flex items-center gap-3 mt-3">
-        <span className="text-sm text-foreground/60">{parsedDate}</span>
+        <span className="text-sm text-foreground/40 transition-all duration-300 hover:text-foreground">
+          {parsedDate}
+        </span>
 
         {team && (
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Link
-                  to={team.url}
-                  target="_blank"
-                  className="flex items-center gap-2 leading-none text-sm text-link"
-                >
-                  {team.name}
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent>Teams</TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
+          <Link
+            to={team.url}
+            target="_blank"
+            className="text-sm text-foreground/40 transition-all duration-300 hover:text-foreground"
+          >
+            {team.name}
+          </Link>
         )}
-      </div>
-
-      <div className="flex flex-wrap items-center gap-4 mt-8 pb-10 border-b-2 border-border border-dashed">
-        <span className="flex items-center gap-1 leading-none! cursor-pointer text-sm font-mono tracking-tight">
-          <i className="fi fi-sr-bullet" />
-          {status}
-        </span>
-
-        <span className="flex items-center gap-1 leading-none! cursor-pointer text-sm font-mono tracking-tight">
-          <i className="fi fi-sr-bullet" />
-          {type}
-        </span>
-
-        <span className="flex items-center gap-1 leading-none! cursor-pointer text-sm font-mono tracking-tight">
-          <i className="fi fi-sr-bullet" />
-          {category}
-        </span>
       </div>
 
       <ArticleContent className="mt-10">
