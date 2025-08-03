65cc04ca3402d9421fa7a6d5662945916c60dafa
Nyoman Sunima <nyomansunima@gmail.com>
2025-08-03 17:28:10 +0800

    fix: add type safety

--- src/app/routes/works.$slug.tsx
+++ src/app/routes/works.$slug.tsx
@@ -1,31 +1,32 @@
 import { generatedMetadata } from "@shared/libs"
 import { getWorkDetail, WorkDetailContent } from "@features/works"
-import type { WorkDetail } from "@features/works"
+import type { Route } from "./+types/works.$slug"
 
-export async function loader({ params }) {
+export async function loader({ params }: Route.LoaderArgs) {
   const { slug } = params
   const work = await getWorkDetail(slug)
 
   return { work }
 }
 
-export function meta({ data }) {
-  const { work } = data
+export function meta({ data }: Route.MetaArgs) {
+  if (data) {
+    const { work } = data
 
-  return generatedMetadata({
-    title: `${work.meta.title} | Nyoman Sunima`,
-    description: work.meta.description,
-    image: work.meta.image,
-  })
+    return generatedMetadata({
+      title: `${work.meta.title} | Nyoman Sunima`,
+      description: work.meta.description,
+      image: work.meta.image,
+    })
+  }
 }
 
-export default function WorkDetailPage({ loaderData }) {
+export default function WorkDetailPage({ loaderData }: Route.ComponentProps) {
   const { work } = loaderData
-  const parsedWork = work as WorkDetail
 
   return (
     <main className="flex flex-col gap-20 tablet:gap-36">
-      <WorkDetailContent work={parsedWork} />
+      <WorkDetailContent work={work} />
     </main>
   )
 }
