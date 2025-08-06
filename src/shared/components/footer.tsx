6793d6b6204fdd707ef38e716c6960a369a21077
Nyoman Sunima <nyomansunima@gmail.com>
2025-08-06 17:36:56 +0800

    refactor: change text size to base

--- src/shared/components/footer.tsx
+++ src/shared/components/footer.tsx
@@ -2,13 +2,13 @@ export function Footer() {
   return (
     <footer className="flex flex-col" id="footer">
       <div className="flex flex-row items-center py-7 justify-between mt-8">
-        <span className="text-sm cursor-pointer inline-flex gap-2">
-          <i className="fi fi-sr-check-circle" />
+        <span className="cursor-pointer inline-flex gap-2">
+          <i className="fi fi-sr-check-circle text-sm" />
           Design and craft in Bali
         </span>
-        <span className="text-sm cursor-pointer inline-flex gap-2">
+        <span className="cursor-pointer inline-flex gap-2">
           Built with
-          <i className="fi fi-sr-heart" />
+          <i className="fi fi-sr-heart text-sm" />
         </span>
       </div>
     </footer>
