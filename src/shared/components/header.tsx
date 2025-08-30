b2ae42d07ed5fb5350f72237b9211bcd97b73b7b
Nyoman Sunima <nyomansunima@gmail.com>
2025-08-30 23:32:14 +0800

    feat: add new theme switcher

--- src/shared/components/header.tsx
+++ src/shared/components/header.tsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router"
 import { Menu } from "./menu"
+import { Button } from "./ui/button"
+import { useTheme } from "next-themes"
 
-export function Brand() {
+function Brand() {
   return (
     <Link
       to={"/"}
@@ -12,11 +14,29 @@ export function Brand() {
   )
 }
 
+function ThemeSwitcher() {
+  const { setTheme, resolvedTheme } = useTheme()
+
+  function switchTheme() {
+    setTheme(resolvedTheme === "light" ? "dark" : "light")
+  }
+
+  return (
+    <Button variant={"secondary"} size={"icon"} onClick={switchTheme}>
+      <i className="fi fi-sr-moon-stars" />
+    </Button>
+  )
+}
+
 export function Header() {
   return (
     <header className="flex items-center justify-between h-24 tablet:h-28">
       <Brand />
-      <Menu />
+
+      <div className="flex items-center gap-2">
+        <ThemeSwitcher />
+        <Menu />
+      </div>
     </header>
   )
 }
