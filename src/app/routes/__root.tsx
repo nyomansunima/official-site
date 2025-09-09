9008e31849b754f550962fc5565b085d02e7eb40
Nyoman Sunima <nyomansunima@gmail.com>
2025-09-09 19:16:29 +0800

    refactor: update the root file

---
+++ src/app/routes/__root.tsx
@@ -0,0 +1,65 @@
+import * as React from "react"
+import {
+  HeadContent,
+  Scripts,
+  createRootRouteWithContext,
+} from "@tanstack/react-router"
+import styles from "@shared/styles/globals.css?url"
+import { ThemeProvider } from "next-themes"
+import { CenteredLayout, Footer, Header } from "@shared/components"
+import type { QueryClient } from "@tanstack/react-query"
+
+interface RootDocumentProps {
+  children: React.ReactNode
+}
+
+interface RouteWithContextProps {
+  queryClient: QueryClient
+}
+
+export const Route = createRootRouteWithContext<RouteWithContextProps>()({
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+    ],
+    links: [
+      {
+        rel: "stylesheet",
+        href: styles,
+      },
+    ],
+  }),
+  shellComponent: RootDocument,
+})
+
+function RootDocument({ children }: RootDocumentProps): React.ReactElement {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        <ThemeProvider
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <CenteredLayout>
+            <Header />
+            <div className="min-h-screen pb-28 mt-5 tablet:mt-10 tablet:pb-56">
+              {children}
+            </div>
+            <Footer />
+          </CenteredLayout>
+        </ThemeProvider>
+        <Scripts />
+      </body>
+    </html>
+  )
+}
