6f48cea019a53ddd42bad321772f4b7d77069e9a
Nyoman Sunima <nyomansunima@gmail.com>
2025-09-11 23:12:41 +0800

    fix: remove meta function

--- src/app/routes/support.tsx
+++ src/app/routes/support.tsx
@@ -2,13 +2,6 @@ import { createFileRoute } from "@tanstack/react-router"
 import { SupportSection } from "@features/support"
 import { generatedMetadata } from "@shared/libs"
 
-export function meta() {
-  return generatedMetadata({
-    title: "Support Me (Donate) | Nyoman Sunima",
-    description: "Support me by donating for projects and contents or goodies",
-  })
-}
-
 export const Route = createFileRoute("/support")({
   head: () => ({
     meta: generatedMetadata({
