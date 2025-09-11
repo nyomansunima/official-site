ee5f8f34d866c6b27e1f7a7e4978e5d2076d4948
Nyoman Sunima <nyomansunima@gmail.com>
2025-09-11 18:31:45 +0800

    refactor: update the services

--- src/features/works/work-service.tsx
+++ src/features/works/work-service.tsx
@@ -3,8 +3,6 @@ import path from "path"
 import { parseMarkdown } from "@shared/libs"
 import { createServerFn } from "@tanstack/react-start"
 
-const WORK_CONTENT_PATH = "src/contents/works"
-
 export interface WorkFrontMatter {
   title: string
   description: string
@@ -26,39 +24,47 @@ export type WorkData = WorkFrontMatter & {
   slug: string
 }
 
+const WORK_DIR_PATH = "src/contents/works"
+
+async function loadWorksDir(): Promise<string[]> {
+  try {
+    const dirPath = path.join(process.cwd(), WORK_DIR_PATH)
+    const files = await fs.readdir(dirPath)
+    const contentFiles = files.filter((file) => file.endsWith(".md"))
+    return contentFiles
+  } catch {
+    throw new Error("Opps, something happen when load the works directory")
+  }
+}
+
+async function loadWorkFile(filePath: string): Promise<string> {
+  try {
+    const file = await fs.readFile(
+      path.join(process.cwd(), WORK_DIR_PATH, filePath),
+      "utf-8",
+    )
+    return file
+  } catch {
+    throw new Error("Opps, no file found")
+  }
+}
+
 export const getWorkDetail = createServerFn({ method: "GET" })
   .validator((data: { slug: string }) => data)
   .handler(async (ctx) => {
-    const slug = ctx.data.slug
-    const filePath = path.join(process.cwd(), WORK_CONTENT_PATH, `${slug}.md`)
-    const file = await fs.readFile(filePath, "utf-8").catch(() => {
-      throw new Response("No work found", {
-        status: 404,
-        statusText:
-          "Opps, the work that you looking is not found. You can find another else",
-      })
-    })
+    const filePath = `${ctx.data.slug}.md`
+    const file = await loadWorkFile(filePath)
     const { content, data } = parseMarkdown(file)
-
     return { meta: data as WorkFrontMatter, content }
   })
 
 export const getWorks = createServerFn({ method: "GET" }).handler(async () => {
-  const workDirPath = path.join(process.cwd(), WORK_CONTENT_PATH)
-  const files = await fs.readdir(workDirPath)
-
-  const contentFiles = files.filter(
-    (file) => file.endsWith(".md") || file.endsWith(".mdx"),
-  )
-
+  const files = await loadWorksDir()
   const works = await Promise.all(
-    contentFiles.map(async (file) => {
-      const filePath = path.join(workDirPath, file)
-
-      const fileContent = await fs.readFile(filePath, "utf-8")
-      const slug = file.replace(/\.(mdx|md)$/, "")
-      const { data } = parseMarkdown(fileContent)
-
+    files.map(async (filePath) => {
+      const slug = filePath.replace(/\.(md)$/, "")
+      const file = await loadWorkFile(filePath)
+      const { data } = parseMarkdown(file)
       return {
         slug,
         ...data,
