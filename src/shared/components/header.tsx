4862ffd406d5d211830b643f1f4d40b3542eea5c
Nyoman Sunima <nyomansunima@gmail.com>
2025-09-20 00:04:47 +0800

    fix: remove menu and bring into header

--- src/shared/components/header.tsx
+++ src/shared/components/header.tsx
@@ -1,7 +1,11 @@
-import { Link } from "@tanstack/react-router"
-import { Menu } from "./menu"
-import { Button } from "./ui/button"
-import { useTheme } from "next-themes"
+import * as React from "react"
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover"
+import { Link, useLocation } from "@tanstack/react-router"
+
+interface NavMenuItemProps {
+  children: React.ReactNode
+  href: string
+}
 
 function Brand() {
   return (
@@ -14,17 +18,48 @@ function Brand() {
   )
 }
 
-function ThemeSwitcher() {
-  const { setTheme, resolvedTheme } = useTheme()
+export function NavMenuItem({ children, href }: NavMenuItemProps) {
+  return (
+    <Link
+      to={href}
+      viewTransition
+      className="flex col-span-1 items-center text-sm transition-all duration-300 text-foreground hover:text-foreground/40"
+    >
+      {children}
+    </Link>
+  )
+}
 
-  function switchTheme() {
-    setTheme(resolvedTheme === "light" ? "dark" : "light")
-  }
+function Menu() {
+  const [open, setOpen] = React.useState(false)
+  const location = useLocation()
+
+  React.useEffect(() => {
+    setOpen(false)
+  }, [location.pathname])
 
   return (
-    <Button variant={"secondary"} size={"icon"} onClick={switchTheme}>
-      <i className="fi fi-sr-moon-stars" />
-    </Button>
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger
+        className="flex justify-center items-center cursor-pointer group relative h-10 w-10 rounded-2xl text-sm bg-secondary text-secondary-foreground"
+        aria-label="Menu Button"
+      >
+        <i className="fi fi-sr-add"></i>
+      </PopoverTrigger>
+      <PopoverContent>
+        <div className="grid grid-cols-2 w-full gap-x-4 gap-y-4">
+          <NavMenuItem href="/works">Works</NavMenuItem>
+          <NavMenuItem href="/collabs">Collabs</NavMenuItem>
+          <NavMenuItem href="/crafts">Crafts</NavMenuItem>
+          <NavMenuItem href="/stories">Stories</NavMenuItem>
+          <NavMenuItem href="/journeys">Journeys</NavMenuItem>
+          <NavMenuItem href="/resources">Resources</NavMenuItem>
+          <NavMenuItem href="/about">About</NavMenuItem>
+          <NavMenuItem href="/contact">Contact</NavMenuItem>
+          <NavMenuItem href="/support">Support</NavMenuItem>
+        </div>
+      </PopoverContent>
+    </Popover>
   )
 }
 
@@ -34,7 +69,6 @@ export function Header() {
       <Brand />
 
       <div className="flex items-center gap-2">
-        <ThemeSwitcher />
         <Menu />
       </div>
     </header>
