35807c31c4939e652cb60415f5fdfe14186d4ee5
Nyoman Sunima <nyomansunima@gmail.com>
2025-09-28 20:47:02 +0800

    refactor: update the router and expose

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,9 +1,9 @@
-import { createRouter as createTanstackRouter } from "@tanstack/react-router"
+import { createRouter } from "@tanstack/react-router"
 import { routeTree } from "./routeTree.gen"
 import { QueryClient } from "@tanstack/react-query"
 import { routerWithQueryClient } from "@tanstack/react-router-with-query"
 
-export const createRouter = () => {
+export function getRouter() {
   // Define the integrations
   // eg. query, etc.
   const queryClient = new QueryClient()
@@ -11,7 +11,7 @@ export const createRouter = () => {
   // Define the router and provide
   // with the context and wrap it
   const router = routerWithQueryClient(
-    createTanstackRouter({
+    createRouter({
       routeTree,
       scrollRestoration: true,
       defaultPreloadStaleTime: 0,
@@ -22,10 +22,4 @@ export const createRouter = () => {
   )
 
   return router
-}
-
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: ReturnType<typeof createRouter>
-  }
 }
