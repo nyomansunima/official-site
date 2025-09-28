ce9a6ceac5c35684bf04f8b9ff7dcc31179d0f84
Nyoman Sunima <nyomansunima@gmail.com>
2025-09-28 20:46:36 +0800

    fix: add the error and not found component

--- src/app/routes/__root.tsx
+++ src/app/routes/__root.tsx
@@ -1,11 +1,12 @@
 import * as React from "react"
 import {
   HeadContent,
+  Link,
   Scripts,
   createRootRouteWithContext,
 } from "@tanstack/react-router"
 import styles from "@shared/styles/globals.css?url"
-import { CenteredLayout, Footer, Header } from "@shared/components"
+import { Button, CenteredLayout, Footer, Header } from "@shared/components"
 import type { QueryClient } from "@tanstack/react-query"
 
 interface RootDocumentProps {
@@ -37,6 +38,8 @@ export const Route = createRootRouteWithContext<RouteWithContextProps>()({
     ],
   }),
   shellComponent: RootDocument,
+  notFoundComponent: NotFondComponent,
+  errorComponent: ErrorComponent,
 })
 
 function RootDocument({ children }: RootDocumentProps): React.ReactElement {
@@ -56,5 +59,63 @@ function RootDocument({ children }: RootDocumentProps): React.ReactElement {
         <Scripts />
       </body>
     </html>
+  )
+}
+
+function NotFondComponent(): React.ReactElement {
+  return (
+    <main className="flex flex-col">
+      <section className="flex flex-col">
+        <div className="flex flex-col">
+          <span className="text-sm text-foreground/40 font-mono tracking-tight">
+            404.
+          </span>
+          <h2 className="text-lg font-medium mt-2">Not found.</h2>
+
+          <p className="mt-6 text-pretty leading-7">
+            Sorry, we couldn’t find the page you’re looking for. It might have
+            been moved or deleted.
+          </p>
+        </div>
+
+        <div className="flex mt-10">
+          <Button asChild variant={"secondary"}>
+            <Link to="/">
+              <i className="fi fi-sr-arrow-alt-circle-left" />
+              Bring me back
+            </Link>
+          </Button>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+function ErrorComponent(): React.ReactElement {
+  return (
+    <main className="flex flex-col">
+      <section className="flex flex-col">
+        <div className="flex flex-col">
+          <span className="text-sm text-foreground/40 font-mono tracking-tight">
+            500.
+          </span>
+          <h2 className="text-lg font-medium mt-2">Error found.</h2>
+
+          <p className="mt-6 text-pretty leading-7">
+            It seems like something went wrong on our end. Please try again
+            later, or contact support if the problem persists.
+          </p>
+        </div>
+
+        <div className="flex mt-10">
+          <Button asChild variant={"secondary"}>
+            <Link to="/">
+              <i className="fi fi-sr-arrow-alt-circle-left" />
+              Bring me back
+            </Link>
+          </Button>
+        </div>
+      </section>
+    </main>
   )
 }
