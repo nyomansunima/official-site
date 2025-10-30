e660325af45ec950811ebcdb33d6a0e9a17d8eb2
Nyoman Sunima <nyomansunima@gmail.com>
2025-10-31 07:26:59 +0800

    refactor: update style

--- src/features/stories/recent-item.tsx
+++ src/features/stories/recent-item.tsx
@@ -15,13 +15,19 @@ export function RecentItem({ recent }: RecentItemProps) {
   const parsedDate = parseReadableDate(timeline);
 
   return (
-    <a className="flex items-center py-2" href={url} target="_blank">
+    <a
+      className="flex tablet:flex-row flex-col tablet:items-center py-2"
+      href={url}
+      target="_blank"
+    >
       <div className="flex grow items-center gap-2">
         <i className="fi fi-sr-bullet text-foreground/40 text-sm" />
         <span className="line-clamp-1 text-pretty">{title}</span>
       </div>
 
-      <span className="text-foreground/40 text-sm">{parsedDate}</span>
+      <span className="ml-5 tablet:ml-0 text-foreground/40 text-sm">
+        {parsedDate}
+      </span>
     </a>
   );
 }
