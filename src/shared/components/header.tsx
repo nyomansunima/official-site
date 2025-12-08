982ba7d54f58b363ac2d9f7afd602e517a8bc62e
Nyoman Sunima <nyomansunima@gmail.com>
2025-12-08 15:05:59 +0800

    feat: add header component

---
+++ src/shared/components/header.tsx
@@ -0,0 +1,29 @@
+type LinkProps = {
+  children: React.ReactNode;
+  href: string;
+};
+
+function Link({ href, children }: LinkProps) {
+  return (
+    <a
+      className="flex items-center justify-center text-foreground/60 text-sm leading-none tracking-normal transition-all duration-300 hover:text-foreground"
+      href={href}
+    >
+      {children}
+    </a>
+  );
+}
+
+export function Header() {
+  return (
+    <header className="mb-16 flex flex-wrap items-center gap-x-7 gap-y-4 border-border border-b border-dashed pt-12 pb-6">
+      <Link href="/">Home</Link>
+      <Link href="/works">Works</Link>
+      <Link href="/products">Products</Link>
+      <Link href="/contents">Contents</Link>
+      <Link href="/journeys">Journeys</Link>
+      <Link href="/about">About</Link>
+      <Link href="/contact">Contact</Link>
+    </header>
+  );
+}
