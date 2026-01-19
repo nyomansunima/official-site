16a2239a63086b7f51d3b51478ed29d22e7cd064
Nyoman Sunima <nyomansunima@gmail.com>
2026-01-19 21:40:53 +0800

    chore: delete about feature

--- src/features/about/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./achievement-section";
-export * from "./activity-section";
-export * from "./endword-section";
-export * from "./indie-section";
-export * from "./intro-section";
-export * from "./sharing-section";
