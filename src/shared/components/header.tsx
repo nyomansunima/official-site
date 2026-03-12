e434ad3fdc40e81220a328572662c7375f2dcfc7
Nyoman Sunima <nyomansunima@gmail.com>
2026-03-13 01:37:16 +0800

    chore: revise the style

--- src/shared/components/header.tsx
+++ src/shared/components/header.tsx
@@ -6,7 +6,7 @@ interface ItemProps {
 function Item({ href, children }: ItemProps) {
   return (
     <a
-      className="text-foreground/60 text-sm transition-colors duration-300 hover:text-foreground"
+      className="text-foreground text-sm transition-colors duration-300 hover:text-foreground/60"
       href={href}
     >
       {children}
