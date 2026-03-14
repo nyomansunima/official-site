360213f9bcaad3f1125afc4ce8fd00344880c3eb
Nyoman Sunima <nyomansunima@gmail.com>
2026-03-15 00:05:32 +0800

    chore: remove the vite tsconfig plugin, use native vite resolve feature

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,12 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [
-    tsconfigPaths(),
     tailwindcss(),
     tanstackStart({
       srcDirectory: "src/app",
@@ -25,4 +23,7 @@ export default defineConfig({
     react(),
     nitro(),
   ],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
