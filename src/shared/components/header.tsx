902fdb626bea65fea9479ae292a1780fb57afeda
Nyoman Sunima <nyomansunima@gmail.com>
2026-03-16 16:59:17 +0800

    chore: revise the style

--- src/shared/components/header.tsx
+++ src/shared/components/header.tsx
@@ -6,7 +6,7 @@ interface ItemProps {
 function Item({ href, children }: ItemProps) {
   return (
     <a
-      className="text-foreground/60 tracking-tighter transition-colors duration-300 hover:text-foreground text-sm"
+      className="text-foreground/60 tracking-tighter transition-colors duration-300 hover:text-foreground"
       href={href}
     >
       {children}
@@ -16,7 +16,7 @@ function Item({ href, children }: ItemProps) {
 
 export function Header() {
   return (
-    <header className="flex items-center gap-5 pt-7 pb-3">
+    <header className="flex items-center gap-5 pt-7 pb-3 border-b border-border border-dashed">
       <Item href="/">Home</Item>
       <Item href="/projects">Projects</Item>
       <Item href="/contents">Contents</Item>
