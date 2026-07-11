f14d6600ac4e83c905c014d90c5915c67ff95d6b
Nyoman Sunima <nyomansunima@gmail.com>
2026-07-11 21:38:28 +0800

    feat: redesign project and contact

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -34,9 +34,23 @@ export function ContactSection() {
         Connects.
       </h1>
 
-      <div className="mt-5 flex flex-col gap-10">
+      <div className="mt-5 flex">
+        <a
+          className="flex w-full cursor-pointer rounded-xl border border-border border-dashed bg-surface p-1 transition-all duration-300 hover:-translate-y-0.5"
+          href={"https://api.whatsapp.com/send?phone=6285161619109"}
+          rel="noopener nooppener"
+          target="_blank"
+        >
+          <img
+            alt="Contact Me"
+            className="h-30 w-full overflow-hidden rounded-lg object-cover object-top"
+            src="https://cdn.dribbble.com/userupload/47926872/file/770ba11804dc2a759461a430192acdaa.png?resize=2048x1536&vertical=center"
+          />
+        </a>
+      </div>
+
+      <div className="mt-3 flex flex-col gap-10">
         <div className="flex flex-col gap-3">
-          <span className="text-foreground/40">Send me favors:</span>
           <div className="grid grid-cols-2 gap-x-5 gap-y-1 sm:grid-cols-3 sm:gap-x-8">
             <Item
               href="https://linkedin.com/in/nyomansunima"
--- src/components/project.tsx
+++ src/components/project.tsx
@@ -2,25 +2,38 @@ import { useMediaQuery } from "~/libs/use-media-query";
 import { Button } from "./button";
 import { Dialog, DialogContent, DialogTrigger } from "./dialog";
 import { Drawer, DrawerContent, DrawerTrigger } from "./drawer";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "./hover-card";
 
 const featured: ProjectData[] = [
   {
     icon: "https://coined.vercel.app/favicon.ico",
     title: "Coined",
     href: "https://www.thecoined.site",
-    desc: "helping business build, ship faster & scale",
+    desc: "Helping business build, ship faster & scale",
   },
   {
     icon: "https://www.poockey.site/favicon.ico",
     title: "Poockey",
     href: "https://www.poockey.site",
-    desc: "all in one tool for personal finances",
+    desc: "All in one tool for personal finances",
   },
   {
     icon: "https://signa.moono.site/favicon.ico",
     title: "Signa",
     href: "https://signa.moono.site",
-    desc: "sign documents with no headache",
+    desc: "Sign documents with no headache",
+  },
+  {
+    icon: "https://s3-new.macosicons.com/macosicons/parse/macOSicons_lfznh2ZAJq_lowResPng-aeeddac5ba.png",
+    title: "Creevoo",
+    href: "https://www.creevoo.store",
+    desc: "Building stunning website templates",
+  },
+  {
+    icon: "https://okay.moono.site/favicon.ico",
+    title: "Okay",
+    href: "https://okay.moono.site",
+    desc: "one tool for business transactions",
   },
 ];
 
@@ -106,53 +119,54 @@ interface ProjectProps {
 
 function Project({ project }: ProjectProps) {
   return (
-    <a
-      className="group relative flex w-full items-center gap-2 outline-none transition-all duration-300"
-      href={project.href}
-      rel="noopener"
-      target="_blank"
-    >
-      <div className="group/image relative h-11 w-11">
-        <span className="absolute h-11 w-11 scale-80 rounded-2xl border border-border border-dashed bg-secondary/70 transition-all duration-300 group-hover:translate-x-1 group-hover:-translate-y-1 group-hover:scale-95" />
-        <div className="absolute rounded-2xl border border-border border-dashed bg-background p-1 transition-all duration-200 group-hover:translate-y-0.5">
-          <img
-            alt={project.title}
-            className="overflow-hidden rounded-lg object-cover transition-all duration-300 group-hover:-rotate-6"
-            height={44}
-            src={project.icon}
-            width={44}
-          />
+    <HoverCard>
+      <HoverCardTrigger
+        render={
+          <a
+            className="group relative flex h-13 w-13 items-center gap-2 outline-none transition-all duration-300"
+            href={project.href}
+            rel="noopener"
+            target="_blank"
+          >
+            <span className="absolute h-13 w-13 rotate-12 scale-80 rounded-2xl border border-border border-dashed bg-background transition-all duration-300 group-hover:translate-x-1 group-hover:translate-y-2 group-hover:scale-95" />
+            <span className="absolute h-13 w-13 scale-80 rounded-2xl border border-border border-dashed bg-background transition-all duration-300 group-hover:translate-x-1 group-hover:translate-y-1 group-hover:scale-95" />
+            <div className="absolute rounded-2xl border border-border border-dashed bg-background p-2 transition-all duration-200 group-hover:-translate-y-0.5">
+              <img
+                alt={project.title}
+                className="overflow-hidden rounded-lg object-cover transition-all duration-300 group-hover:-rotate-6"
+                height={44}
+                src={project.icon}
+                width={44}
+              />
+            </div>
+          </a>
+        }
+      />
+      <HoverCardContent>
+        <div className="relative flex select-none flex-col">
+          <h3 className="text-foreground">{project.title}</h3>
+          <span className="text-foreground/40">{project.desc}</span>
+
+          <svg
+            className="absolute -top-2 -right-2 text-foreground/20"
+            fill="currentColor"
+            height={14}
+            viewBox="0 0 24 24"
+            width={14}
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0 0h24v24H0z" fill="none" stroke="none" />
+            <path d="M12 7a5 5 0 1 1 -4.995 5.217l-.005 -.217l.005 -.217a5 5 0 0 1 4.995 -4.783z" />
+          </svg>
         </div>
-      </div>
-
-      <div className="flex flex-col">
-        <h3 className="leading-tight transition-all duration-300 group-hover:text-foreground">
-          {project.title}
-        </h3>
-        <span className="text-foreground/40">{project.desc}</span>
-      </div>
-
-      <svg
-        className="absolute right-0 -rotate-45 text-foreground/40 opacity-0 transition-all delay-200 duration-300 group-hover:text-foreground group-hover:opacity-100"
-        fill="currentColor"
-        height={16}
-        viewBox="0 0 24 24"
-        width={16}
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M0 0h24v24H0z" fill="none" stroke="none" />
-        <path
-          d="M12 2c-.218 0 -.432 .002 -.642 .005l-.616 .017l-.299 .013l-.579 .034l-.553 .046c-4.785 .464 -6.732 2.411 -7.196 7.196l-.046 .553l-.034 .579c-.005 .098 -.01 .198 -.013 .299l-.017 .616l-.004 .318l-.001 .324c0 .218 .002 .432 .005 .642l.017 .616l.013 .299l.034 .579l.046 .553c.464 4.785 2.411 6.732 7.196 7.196l.553 .046l.579 .034c.098 .005 .198 .01 .299 .013l.616 .017l.642 .005l.642 -.005l.616 -.017l.299 -.013l.579 -.034l.553 -.046c4.785 -.464 6.732 -2.411 7.196 -7.196l.046 -.553l.034 -.579c.005 -.098 .01 -.198 .013 -.299l.017 -.616l.005 -.642l-.005 -.642l-.017 -.616l-.013 -.299l-.034 -.579l-.046 -.553c-.464 -4.785 -2.411 -6.732 -7.196 -7.196l-.553 -.046l-.579 -.034a28.058 28.058 0 0 0 -.299 -.013l-.616 -.017l-.318 -.004l-.324 -.001zm.613 5.21l.094 .083l4 4a.927 .927 0 0 1 .097 .112l.071 .11l.054 .114l.035 .105l.03 .148l.006 .118l-.003 .075l-.017 .126l-.03 .111l-.044 .111l-.052 .098l-.074 .104l-.073 .082l-4 4a1 1 0 0 1 -1.497 -1.32l.083 -.094l2.292 -2.293h-5.585a1 1 0 0 1 -.117 -1.993l.117 -.007h5.585l-2.292 -2.293a1 1 0 0 1 -.083 -1.32l.083 -.094a1 1 0 0 1 1.32 -.083z"
-          fill="currentColor"
-        />
-      </svg>
-    </a>
+      </HoverCardContent>
+    </HoverCard>
   );
 }
 
 function ProjectList() {
   return (
-    <div className="flex w-full flex-col gap-3">
+    <div className="flex w-full justify-between gap-3">
       {featured.map((project, i) => (
         <Project key={i} project={project} />
       ))}
@@ -167,7 +181,7 @@ function MoreProjects() {
         Projects.
       </h2>
       <div className="no-scrollbar max-h-[65vh] w-full overflow-y-auto py-3 sm:max-h-[60vh]">
-        <div className="flex w-full flex-col gap-3">
+        <div className="flex w-full flex-wrap gap-6">
           {projects.map((project, i) => (
             <Project key={i} project={project} />
           ))}
